53c03049268f4fcea5e6980e1b3209936171fce1
Igweonu Ebuka <igweonuebukahazard@gmail.com>
2022-07-13 23:19:33 -1100

    first commit

---
+++ boolean.js
@@ -0,0 +1,8 @@
+var instock;
+var shipping;
+instock = true;
+shipping = false;
+var elstock = document.getElementById('stock');
+elstock.classname = instock;
+var elship = document.getElementById('shipping');
+elship.className = shipping;
